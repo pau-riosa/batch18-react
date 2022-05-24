2c260da41e874b4def82847d6864e97f404c5ea6
pau <jethro.riosa@gmail.com>
2022-05-24 20:13:40 +0800

    create login component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Form from "./components/Form.js";
 import List from "./components/List.js";
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+  const [userName, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [items, setItems] = useState([]);
 
   // add item
@@ -30,15 +33,66 @@ function App() {
     });
   };
 
+  const loginHandler = (event) => {
+    event.preventDefault();
+
+    const data = {
+      userName: userName,
+      password: password,
+    };
+
+    const api_body = JSON.stringify(data);
+
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "X-RapidAPI-Host": "fake-authentication1.p.rapidapi.com",
+        "X-RapidAPI-Key": "<SIGN-UP-FOR-API-KEY>",
+      },
+      body: api_body,
+    };
+
+    fetch(
+      "https://fake-authentication1.p.rapidapi.com/api/v1/authentication/login",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        setIsLogin(true);
+        console.log(response);
+      })
+      .catch((err) => {
+        console.error(err);
+        setIsLogin(false);
+      });
+  };
+
   return (
     <div className="App">
-      <h1>Add a Todo</h1>
-      <div>
-        <Form add={addItem} />
-      </div>
-      <div>
-        <List items={items} delete={deleteItem} update={updateItem} />
-      </div>
+      {isLogin ? (
+        <>
+          <h1>Add a Todo</h1>
+          <Form add={addItem} />
+          <List items={items} delete={deleteItem} update={updateItem} />
+        </>
+      ) : (
+        <>
+          <form onSubmit={loginHandler}>
+            <input
+              type="text"
+              value={userName}
+              onChange={(event) => setUsername(event.target.value)}
+            />
+            <input
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+            <input type="submit" value="Login" />
+          </form>
+        </>
+      )}
     </div>
   );
 }
